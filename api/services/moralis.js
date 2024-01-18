848ce57e9ec9b744a134d02acbb5b33f7d7bb5d7
MichalisG <michalis3489@gmail.com>
2024-01-18 19:21:22 +0200

    feat: helper functions to load data from moralis api

---
+++ api/services/moralis.js
@@ -0,0 +1,24 @@
+import Moralis from 'moralis';
+
+import { EvmChain } from '@moralisweb3/common-evm-utils';
+
+const defaultChain = EvmChain.ETHEREUM;
+
+export const getContactNfts = async (address, cursor, chain = defaultChain) => {
+  return await Moralis.EvmApi.nft.getContractNFTs({
+    chain,
+    address,
+    cursor
+  });
+}
+
+export const getNft = async (address, tokenId, chain = defaultChain) => {
+  return await Moralis.EvmApi.nft.getNFTMetadata({
+    chain,
+    address,
+    tokenId,
+    normalizeMetadata: true,
+    mediaItems: true,
+
+  });
+}
