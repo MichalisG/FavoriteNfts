d899dc7e1f316bf5e277be75a8cdd51fca5bb5b9
MichalisG <michalis3489@gmail.com>
2024-01-19 19:48:47 +0200

    feat: collection interface

--- ui/src/interfaces/nft.tsx
+++ ui/src/interfaces/nft.tsx
@@ -1,3 +1,5 @@
+import Collection from "@@/components/Collection";
+
 export interface MediaCollection {
   width: number;
   height: number;
@@ -24,4 +26,23 @@ export interface NFT {
   token_uri: string;
   metadata?: string;
   media?: Media;
+}
+
+export interface Collection  {
+  token_address: string;
+  name: string;
+  symbol: string;
+  contract_type: string;
+  synced_at: string;
+  possible_spam: boolean;
+  verified_collection: boolean;
+  collection_logo: string;
+  collection_banner_image?: string | null;
+  collection_category: string | null;
+  project_url?: string | null;
+  wiki_url?: string | null;
+  discord_url?: string | null;
+  telegram_url?: string | null;
+  twitter_username?: string | null;
+  instagram_username?: string | null;
 }
