122251acd22df44ce5916776a22854095ca12031
MichalisG <michalis3489@gmail.com>
2024-01-19 18:24:58 +0200

    feat: favorites page

--- ui/src/app/favorites/page.tsx
+++ ui/src/app/favorites/page.tsx
@@ -1,12 +1,27 @@
-import Image from 'next/image'
+'use client'
 
+import React, {useState} from 'react';
+import Favorites from '@@/components/Favorites';
 
-const Favorites = () => {
+const Browse = () => {
+
+  const onCollectionAddressChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    // TODO validate that it is a valid ethereum address
+    setCollectionAddress(event.target.value);
+  }
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      Favorites
+    <div className="flex min-h-screen flex-col items-center p-24">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-4xl font-bold">All your Favorites NFTs</h1>
+        </div>
+      </div>
+      <div className="py-6">
+        <Favorites/>
+      </div>
     </div>
   )
 }
 
-export default Favorites;
+export default Browse;
