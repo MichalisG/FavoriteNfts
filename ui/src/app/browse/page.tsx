84c9834703a66b0bde02ef21226490dc392e4317
MichalisG <michalis3489@gmail.com>
2024-01-19 19:46:33 +0200

    feat: show collection info

--- ui/src/app/browse/page.tsx
+++ ui/src/app/browse/page.tsx
@@ -2,13 +2,21 @@
 
 import React, {useState} from 'react';
 import Collection from '@@/components/Collection';
+import { useSearchParams, useRouter } from 'next/navigation'
+import useCollectionMetadata from '@@/hooks/useCollectionMetadata';
+import Image from 'next/image';
 
 const Browse = () => {
-  const [collectionAddress, setCollectionAddress] = useState<string>('' as string);
+  const searchParams = useSearchParams()
+  const { replace } = useRouter();
+  const { collection } = useCollectionMetadata({address: searchParams.get('address') || ''})
+
+  const collectionAddress = searchParams.get('address') || '';
 
   const onCollectionAddressChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    // TODO validate that it is a valid ethereum address
-    setCollectionAddress(event.target.value);
+    const params = new URLSearchParams(searchParams);
+    params.set('address', event.target.value);
+    replace(`?${params.toString()}`)
   }
 
   return (
@@ -21,6 +29,20 @@ const Browse = () => {
       </div>
       <h4 className="text-2xl font-bold py-4">Enter Collection Address</h4>
       <input value={collectionAddress} type="text" placeholder="Address" className="input input-bordered w-full" onChange={onCollectionAddressChange}/>
+      <div className='w-full mt-20 text-center'>
+        {
+          collection?.collection_banner_image ? <Image
+            className="h-56 w-full object-cover"
+            src={collection?.collection_banner_image}
+            alt={'banner'}
+            unoptimized
+            width={500}
+            height={500}
+          /> : null
+        }
+
+        <h2 className="text-4xl font-bold py-4 mt-8 text-center">{collection?.name}</h2>
+      </div>
       <div className="py-6">
         <Collection collectionAddress={collectionAddress}/>
       </div>
