6fd2c0f11639177ac37bb28842ae5ae4cee71186
MichalisG <michalis3489@gmail.com>
2024-01-19 12:43:22 +0200

    feat: pages

---
+++ ui/src/app/browse/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+const Browse = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      Browse
+    </div>
+  )
+}
+
+export default Browse;
---
+++ ui/src/app/favorites/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+
+const Favorites = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      Favorites
+    </div>
+  )
+}
+
+export default Favorites;
