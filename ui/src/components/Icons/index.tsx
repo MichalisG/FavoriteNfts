db342935bdbb46ec742d43d870dd3eb52ec8c727
MichalisG <michalis3489@gmail.com>
2024-01-19 18:25:29 +0200

    feat: icons component

---
+++ ui/src/components/Icons/index.tsx
@@ -0,0 +1,11 @@
+import Heart from "./Heart";
+import HeartSolid from "./HeartSolid";
+
+export default class Icons {
+  static Heart() {
+    return <Heart />
+  }
+  static HeartSolid() {
+    return <HeartSolid />
+  }
+}
