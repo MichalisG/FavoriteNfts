4565e4c41b54b7fda2ada36db6ace9ad848fcf9c
MichalisG <michalis3489@gmail.com>
2024-01-19 18:23:33 +0200

    fix: post and add a delete endpoint

--- api/routes/favorites.js
+++ api/routes/favorites.js
@@ -5,33 +5,58 @@ import {authenticateJWT} from '../middlewares/auth.js';
 const router  = express.Router();
 
 router.get('/', authenticateJWT, async (req, res) => {
-  const {
-    page = 1,
-    pageSize = 10,
-  } = req.query;
-  const {account} = req.user;
+  try{
 
-  const userFavorites = await Favorite.find({ ethereumAccount: account })
-      
-  res.json(userFavorites);
+    const {
+      page = 1,
+      pageSize = 10,
+    } = req.query;
+    const {account} = req.user;
+    
+    const userFavorites = await Favorite.find({ ethereumAccount: account })
+  
+    res.json(userFavorites.map(favorite => favorite.favoriteNfts[0]));
+  }catch(e){
+    console.log(e)
+    res.status(500).send();
+  }
 })
 
 router.post('/', authenticateJWT, async (req, res) => {
-  const { nft } = req.body;
-  const { account } = req.user;
+  try {
+    const { nft } = req.body;
+    const { account } = req.user;
+    
+    const favoriteNFT = new Favorite({
+      ethereumAccount: account,
+      favoriteNfts: [nft]
+    });
+    
+    await favoriteNFT.save();
+    
+    res.status(201).send();
+  } catch(e) {
+    console.log("🚀 ~ router.post ~ e:", e)
+    res.status(500).send();
+  }
+})
 
-  Favorite.updateOne(
-    { ethereumAccount: account },
-    { $addToSet: { favoriteNfts: nft } },
-    { upsert: true },
-    (err, result) => {
-      if (err) throw err;
-
-      res.status(201).send();
-    }
-  );
-
-  res.status(201).send();
+router.delete('/', authenticateJWT, async (req, res) => {
+  try {
+    const { nft } = req.body;
+    const { account } = req.user;
+    
+    await Favorite.deleteOne({
+      ethereumAccount: account,
+      'favoriteNfts.token_address': nft.token_address,
+      'favoriteNfts.token_id': nft.token_id,
+    });
+    
+    res.status(201).send();
+  } catch(e) {
+    console.log("🚀 ~ router.post ~ e:", e)
+    res.status(500).send();
+  }
 })
 
 export default router;
