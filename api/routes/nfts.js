4560d3e08db0ffe2fd7fe9342d8058dc22bae937
MichalisG <michalis3489@gmail.com>
2024-01-19 18:23:43 +0200

    fix; add try catch

--- api/routes/nfts.js
+++ api/routes/nfts.js
@@ -5,19 +5,29 @@ import {authenticateJWT} from '../middlewares/auth.js';
 const nftsRouter  = express.Router();
 
 nftsRouter.get('/:address', authenticateJWT, async (req, res) => {
-  const {address} = req.params;
-  const {chain, cursor} = req.query;
-  const data = await getContactNfts(address, cursor, chain);
-
-  res.json(data);
+  try {
+    const {address} = req.params;
+    const {chain, cursor} = req.query;
+    const data = await getContactNfts(address, cursor, chain);
+    
+    res.json(data);
+  }catch (e) {
+    console.log(e)
+    res.status(500).send();
+  }
 });
 
 nftsRouter.get('/:address/:tokenId', authenticateJWT, async (req, res) => {
-  const {address, tokenId} = req.params;
-  const {chain} = req.query;
-  const data = await getNft(address, tokenId, chain);
-
-  res.json(data);
+  try {
+    const {address, tokenId} = req.params;
+    const {chain} = req.query;
+    const data = await getNft(address, tokenId, chain);
+    
+    res.json(data);
+  } catch (e) {
+    console.log(e)
+    res.status(500).send();
+  }
 });
 
 export default nftsRouter;
